e603548d241c0861cdd44469ce7ac2836383704e
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-10-16 00:02:32 -0700

    Complete basic React setup

---
+++ client/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+class Home extends React.Component{
+  render() {
+    return <div> REACT TEST</div>
+  }
+}
+
+ReactDOM.render(<Home />, document.getElementById('app'));
